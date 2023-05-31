e324cf1fb01f3967aba1dea376c6b089ad2605ed
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2023-05-31 08:14:45 -0300

    Add files via upload

--- questao2.py
+++ questao2.py
@@ -1,7 +1,11 @@
+# Questão 2 - Crie um programa que exibe ao usuário um menu com as seguintes opções: 
+# adicionar número na fila; remover número da fila; tamanho da fila; mostrar fila. 
+# Todas as opções devem funcionar conforme a ação que ela descreve.
+
 from fila import Fila
 
-
 fila = Fila()
+
 while True:
     
     print('Escolha uma opção:')
@@ -15,23 +19,15 @@ while True:
         case 1:
             fila.enqueue(int(input('Valor: ')))
         case 2:
-            valor = int(input('Valor: '))
-            
-            if valor in fila:
-                fila.dequeue(valor)
-        
-            print('Valor não encontrado.')
+            print(f'Removido: {fila.dequeue()}')
         case 3:
-            
             if fila.isEmpty():
                 print('Fila vazia.')
-            
-            fila.peek()
+            fila.mostrar()
         case 4:
             print('Programa encerrado.')
             break
         case _:
             print('Opção inválida.')
             opcao = int(input('Opção: '))
-    
         
